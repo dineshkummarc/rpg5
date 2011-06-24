887f18d8a33fd445beda23cae53a8c7e0690d641
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2011-06-24 23:54:53 +0200

    Random direction IA

--- js/game.js
+++ js/game.js
@@ -1,18 +1,21 @@
 
 (function(window, undefined){
 
+    /***************************************
+                    MAP LOADING
+     ***************************************/
 
     var loadedmap = [
 	           	new Array(2, 2 ,2, 2, 2, 2, 2, 2, 2),
-	           	new Array(2, 1 ,0, 0, 0, 0, 0, 0, 2),
+	           	new Array(2, 1 ,0, 4, 0, 0, 0, 0, 2),
 	           	new Array(2, 0 ,1, 0, 0, 0, 0, 0, 2),
-	           	new Array(2, 0 ,0, 1, 0, 0, 0, 0, 2),
+	           	new Array(2, 0 ,0, 1, 0, 4, 0, 0, 2),
 	           	new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
-	           	new Array(2, 0 ,0, 4, 0, 0, 0, 0, 2),
-                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
-                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
-                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
-                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
+	           	new Array(2, 0 ,0, 4, 0, 0, 4, 0, 2),
+                new Array(2, 0 ,0, 0, 4, 0, 0, 0, 2),
+                new Array(2, 0 ,4, 0, 0, 4, 0, 0, 2),
+                new Array(2, 0 ,4, 0, 0, 0, 0, 0, 2),
+                new Array(2, 0 ,0, 0, 4, 0, 0, 0, 2),
                 new Array(2, 2 ,2, 2, 2, 2, 2, 2, 2)
 	           ];
 
@@ -26,7 +29,7 @@
             currentBlock.className = 'block' + ' bx' + i + ' by' + j + ' t' + loadedmap[i][j];
             if (loadedmap[i][j] == 4){
                 currentBlock.className += ' mob';
-                enemies.push({x : i * 50, y : j * 50, div : currentBlock});
+                enemies.push({x : i * 50, y : j * 50, dir : Math.floor(Math.random() * 4) ,div : currentBlock});
 
             }
             blocksLine.appendChild(currentBlock);
@@ -35,6 +38,9 @@
     }
 
 
+    /***************************************
+                    GAME INIT
+     ***************************************/
 
 
     var player = {
@@ -49,6 +55,11 @@
     playerdiv.style.left = player.x + 'px';
     playerdiv.style.top = player.y + 'px';
     game.appendChild(playerdiv);
+
+    /***************************************
+                    CONTROLS
+     ***************************************/
+
 
     var oMove = {up:0, down:0, left:0, right:0};
 
@@ -68,9 +79,7 @@ window.addEventListener("keyup", function(e){
     }, false);
 
 
-setInterval(mainLoop, 16);
-
-function mainLoop(){
+function playerMoves(){
     if (oMove.down)     player.y = player.y + 2;
     if (oMove.up)       player.y = player.y - 2;
     if (oMove.left)     player.x = player.x - 2;
@@ -80,8 +89,36 @@ function mainLoop(){
         playerdiv.style.top = player.y + 'px';
     if (oMove.left || oMove.right)
         playerdiv.style.left = player.x + 'px';
-    enemies[0].x++;
-    enemies[0].div.style.left = enemies[0].x + 1 + 'px';
+};
+
+    /***************************************
+                    IA
+     ***************************************/
+
+function enemiesMoves(){
+    for (var i = 0, l = enemies.length; i < l; ++i){
+        if (enemies[i].dir == 0) enemies[i].y--;
+        if (enemies[i].dir == 1) enemies[i].x++;
+        if (enemies[i].dir == 2) enemies[i].y++;
+        if (enemies[i].dir == 3) enemies[i].x--;
+
+
+        enemies[i].div.style.left = enemies[i].x + 'px';
+        enemies[i].div.style.top = enemies[i].y + 'px';
+
+    }
+}
+
+    /***************************************
+                    MAIN LOOP
+     ***************************************/
+
+
+setInterval(mainLoop, 16);
+
+function mainLoop(){
+    playerMoves();
+    enemiesMoves();
 
 
 /*
