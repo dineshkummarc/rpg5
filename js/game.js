32b29851c3aeb190382fd6841cfd608230a35aea
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2011-06-24 23:42:31 +0200

    Added a mob

--- js/game.js
+++ js/game.js
@@ -3,19 +3,20 @@
 
 
     var loadedmap = [
-	           	new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-	           	new Array(0, 1 ,0, 0, 0, 0, 0, 0, 0),
-	           	new Array(0, 0 ,1, 0, 0, 0, 0, 0, 0),
-	           	new Array(0, 0 ,0, 1, 0, 0, 0, 0, 0),
-	           	new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-	           	new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-                new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-                new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-                new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-                new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0),
-                new Array(0, 0 ,0, 0, 0, 0, 0, 0, 0)
+	           	new Array(2, 2 ,2, 2, 2, 2, 2, 2, 2),
+	           	new Array(2, 1 ,0, 0, 0, 0, 0, 0, 2),
+	           	new Array(2, 0 ,1, 0, 0, 0, 0, 0, 2),
+	           	new Array(2, 0 ,0, 1, 0, 0, 0, 0, 2),
+	           	new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
+	           	new Array(2, 0 ,0, 4, 0, 0, 0, 0, 2),
+                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
+                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
+                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
+                new Array(2, 0 ,0, 0, 0, 0, 0, 0, 2),
+                new Array(2, 2 ,2, 2, 2, 2, 2, 2, 2)
 	           ];
 
+    var enemies = [];
     var scene = document.getElementById('game');
     for (var i = 0, l = loadedmap.length; i < l; ++i){
         var blocksLine = document.createElement('div');
@@ -23,10 +24,17 @@
         for (var j = 0, m = loadedmap[0].length; j < m; ++j){
             var currentBlock = document.createElement('div');
             currentBlock.className = 'block' + ' bx' + i + ' by' + j + ' t' + loadedmap[i][j];
+            if (loadedmap[i][j] == 4){
+                currentBlock.className += ' mob';
+                enemies.push({x : i * 50, y : j * 50, div : currentBlock});
+
+            }
             blocksLine.appendChild(currentBlock);
         }
         scene.appendChild(blocksLine);
     }
+
+
 
 
     var player = {
@@ -63,13 +71,25 @@ window.addEventListener("keyup", function(e){
 setInterval(mainLoop, 16);
 
 function mainLoop(){
-    if (oMove.down) player.y++;
-if (oMove.up) player.y--;
-if (oMove.left) player.x--;
-if (oMove.right) player.x++;
+    if (oMove.down)     player.y = player.y + 2;
+    if (oMove.up)       player.y = player.y - 2;
+    if (oMove.left)     player.x = player.x - 2;
+    if (oMove.right)    player.x = player.x + 2;
 
+    if (oMove.down || oMove.up)
+        playerdiv.style.top = player.y + 'px';
+    if (oMove.left || oMove.right)
         playerdiv.style.left = player.x + 'px';
-    playerdiv.style.top = player.y + 'px';
+    enemies[0].x++;
+    enemies[0].div.style.left = enemies[0].x + 1 + 'px';
+
+
+/*
+    for (var currentEnemy in enemies){
+        currentEnemy.x++;
+        currentEnemy.div.style.left = currentEnemy.x;
+    }
+*/
 }
 
 })(window);
